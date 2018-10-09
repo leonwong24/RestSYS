42ca171ff4ac6fb009402c42a94d7f4f8f80fbd5
Leon Wong <t00198005@365s.ittralee.ie>
2018-10-09 14:46:46 +0100

    First stage of validation (add food item) done

--- RestSYS/frmAddFoodItem.cs
+++ RestSYS/frmAddFoodItem.cs
@@ -19,13 +19,37 @@ namespace RestSYS
 
         private void btnAddFoodItemSubmit_Click(object sender, EventArgs e)
         {
-            //validate data 
+            //validate data
+            if (txtAddFoodItem.Text.Equals(""))
+            {
+                MessageBox.Show("Food Item name must be entered", "Error",MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            else if (txtAddFoodItemDesc.Text.Equals(""))
+            {
+                MessageBox.Show("Food item description must be entered", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            else if (txtAddFoodItemType.Text.Equals(""))
+            {
+                MessageBox.Show("Food item type must be entered", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            else if (txtAddFoodItemPrice.Text.Equals(""))
+            {
+                MessageBox.Show("Food item price must be entered", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            else if (txtAddFoodItemStatus.Text.Equals(""))
+            {
+                MessageBox.Show("Food item status must be entered", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
 
             //display confirmation message 
 
-            //save food item details 
+                //save food item details 
 
-            //clear the ui
+                //clear the ui
         }
     }
 }
